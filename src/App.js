360611ca20781553e4cf89e298539b7ccf60a305
Bahtiyorjon Arslonbekov <bahtiyorjonarslonbekov@Bahtiyorjons-MacBook-Air.local>
2021-08-27 17:20:45 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import React from "react";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Let's build the FaceBook clone</h1>
+      {/* Header */}
+      <Header />
+      {/* App body */}
+      {/* SideBar */}
+      {/* Feed */}
+      {/* Widgets */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDk1APnJBLZap7mMVakRtK4EmerXIOKDf8",
+  authDomain: "fb-clone-7d56d.firebaseapp.com",
+  projectId: "fb-clone-7d56d",
+  storageBucket: "fb-clone-7d56d.appspot.com",
+  messagingSenderId: "997481480019",
+  appId: "1:997481480019:web:959bcec5b2edd52e50eba6",
+  measurementId: "G-6C9TTYHQJV",
+};
