ac3f4ea103dfbc90bb4241c6dc98dd7715788ad8
Bahtiyorjon Arslonbekov <bahtiyorjonq777@gmail.com>
2021-09-02 00:26:29 +0500

    added post

--- src/Feed/Feed.js
+++ src/Feed/Feed.js
@@ -2,12 +2,15 @@ import React from "react";
 import "./Feed.css";
 import MessageSender from "./MessageSender/MessageSender";
 import StoryWheel from "./StoryWheel/StoryWheel";
+import Post from "./Post/Post";
 
 const Feed = () => {
   return (
     <div className="feed">
       <StoryWheel />
       <MessageSender />
+      <Post />
+      <Post />
       <Post />
     </div>
   );
