694ab572435bbf1ea65f7921ed5848c9d150b56a
Bahtiyorjon Arslonbekov <bahtiyorjonq777@gmail.com>
2021-09-02 01:20:00 +0500

    post ready

--- src/Feed/Post/Post.js
+++ src/Feed/Post/Post.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { Avatar } from "@material-ui/core";
 import "./Post.css";
+import ThumbUpIcon from "@material-ui/icons/ThumbUp";
+import { AccountCircle, ChatBubbleOutline } from "@material-ui/icons";
+import { NearMe } from "@material-ui/icons";
+import { ExpandMoreOutlined } from "@material-ui/icons";
 
 const Post = ({ profilePic, image, username, timestamp, message }) => {
   return (
@@ -18,6 +22,24 @@ const Post = ({ profilePic, image, username, timestamp, message }) => {
       </div>
       <div className="post__image">
         <img src={image} alt="" />
+      </div>
+      <div className="post__options">
+        <div className="post__option">
+          <ThumbUpIcon />
+          <p>Like</p>
+        </div>
+        <div className="post__option">
+          <ChatBubbleOutline />
+          <p>Comment</p>
+        </div>
+        <div className="post__option">
+          <NearMe />
+          <p>Share</p>
+        </div>
+        <div className="post__option">
+          <AccountCircle />
+          <ExpandMoreOutlined />
+        </div>
       </div>
     </div>
   );
