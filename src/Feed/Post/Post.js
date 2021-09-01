77c0f3bd149886a8cc5de521244934601ff29c97
Bahtiyorjon Arslonbekov <bahtiyorjonq777@gmail.com>
2021-09-02 00:31:11 +0500

    post avatar added

--- src/Feed/Post/Post.js
+++ src/Feed/Post/Post.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { Avatar } from "@material-ui/core";
 import "./Post.css";
 
-const Post = () => {
-  return <div className="post"></div>;
+const Post = ({ profilePic, image, username, timestamp, message }) => {
+  return (
+    <div className="post">
+      <div className="post__top">
+        <Avatar src={profilePic} className="post__avatar" />
+      </div>
+    </div>
+  );
 };
 
 export default Post;
