18fb3b9917ebebee9ddb2ce48c9622b3ab95a5da
Bahtiyorjon Arslonbekov <bahtiyorjonq777@gmail.com>
2021-09-03 15:21:46 +0500

    next

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ import Login from "./Login/Login";
 import useStateValue from "./StateProvider";
 
 function App() {
-  const [state, dispatch] = useStateValue();
-  const user = null;
+  const [{ user }, dispatch] = useStateValue();
+
   return (
     <div className="app">
       {!user ? (
--- src/StateProvider.js
+++ src/StateProvider.js
@@ -3,11 +3,15 @@ import React, { createContext, useContext, useReducer } from "react";
 export const StateContext = createContext();
 
 export const StateProvider = ({ reducer, initialState, children }) => {
-  <StateContext.Provider value={useReducer(reducer, initialState)}>
-    {children}
-  </StateContext.Provider>;
+  return (
+    <StateContext.Provider value={useReducer(reducer, initialState)}>
+      {children}
+    </StateContext.Provider>
+  );
 };
 
-export default useStateValue = () => {
+const useStateValue = () => {
   useContext(StateContext);
 };
+
+export default useStateValue;
